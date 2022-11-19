7d1cf50239f0d7a40f4cd015c3294a5bf267c782
NuochengLin <nuochel1@uci.edu>
2022-11-18 23:27:06 -0800

    massively improve efficiency by extract the shelve opening process out of fuction

--- query.py
+++ query.py
@@ -3,9 +3,9 @@ from nltk.stem.snowball import SnowballStemmer
 import re, math
 import os
 import heapq
+import time
 
-index_path = "./index"  # data source directory
-file_dir = "./DEV"  # index folder
+
 # def intersect(*postings):
 #     if len(postings) <= 1:
 #         return postings[0]
@@ -29,33 +29,47 @@ file_dir = "./DEV"  # index folder
 #                 b1 = next(b, None)
 #         return intersect(*([result] + list(postings[2:])))
 
-def ranking_query(query):
-    pattern = re.compile(r"[a-zA-Z0-9]+")
-    stemmer = SnowballStemmer("english")
-    with shelve.open(os.path.join(index_path, 'inverted_index'), flag='c', writeback=True) as index:
-        with shelve.open(os.path.join(file_dir, 'id_url'), flag='c') as mapping:
-            token_list = set()
-            candidate = set()
-            for t in pattern.finditer(query):
-                word = stemmer.stem(t.group(0))
-                if word in index:
-                    for d in index[word]:
-                        if d != "idf":
-                            candidate.add(d)
-                    token_list.add(word)
-            # bool_result = intersect(*sorted(token_list, key= lambda x:len(x)))
-            # print(bool_result)
-            print(len(candidate))
-            heap = []
-            for d in candidate:
-                contain = 0
-                score = 0
-                for term in token_list:
-                    contain += 1
-                    score += index[term][d]["w"] * index[term]["idf"]
-                if contain/len(token_list) >= 0.75:
-                    heapq.heappush(heap, (-score, d))
-            for score, id in heap[:10]:
-                print(id, ": ", mapping[id], "with score: ",-score, end = "\n")
-query = input("Please enter your query: ")
-ranking_query(query)
+def ranking_query(query, index, mapping):
+    token_list = set()
+    candidate = set()
+    for word in query:
+        if word in index and index[word]["idf"] > 0.2:
+            for d in index[word]:
+                if d != "idf":
+                    candidate.add(d)
+            token_list.add(word)
+    # bool_result = intersect(*sorted(token_list, key= lambda x:len(x)))
+    # print(bool_result)
+    heap = []
+    for d in candidate:
+        contain = 0
+        score = 0
+        for term in token_list:
+            contain += 1
+            if d in index[term]:
+                score += index[term][d]["w"] * index[term]["idf"]
+        if contain / len(token_list) >= 0.75:
+            heapq.heappush(heap, (-score, d))
+    for score, id in heap[:5]:
+        print(id, ": ", mapping[id], "with score: ", -score, end="\n")
+
+
+if __name__ == "__main__":
+    with shelve.open(os.path.join("./index", 'inverted_index'), flag='c', writeback=True) as index:
+        with shelve.open(os.path.join("./index", 'id_url'), flag='c') as mapping:
+            pattern = re.compile(r"[a-zA-Z0-9]+")
+            stemmer = SnowballStemmer("english")
+            query_list = ["cristina lopes", "machine learning", "ACM", "master of software engineering"]
+            for query in query_list:
+                print("----------------------------------")
+                print("start processing query", query, ": ")
+                token_list = []
+
+                s = time.time()
+                for t in pattern.finditer(query):
+                    token_list.append(stemmer.stem(t.group(0)))
+                ranking_query(token_list, index, mapping)
+                end = time.time()
+
+                print(query, "takes: ", end - s)
+                print()
