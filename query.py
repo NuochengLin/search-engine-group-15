23db1a7fc896d6032fd5256fe706fbdd9a11fb3a
NuochengLin <nuochel1@uci.edu>
2022-11-18 20:21:07 -0800

    ranking query

--- query.py
+++ query.py
@@ -3,35 +3,57 @@ from nltk.stem.snowball import SnowballStemmer
 import re, math
 import os
 import heapq
-from numpy import array
+
+
+# def intersect(*postings):
+#     if len(postings) <= 1:
+#         return postings[0]
+#     else:
+#         result = []
+#         a = iter(postings[0])
+#         b = iter(postings[1])
+#         if not (a and b):
+#             print("fail")
+#             return []
+#         a1 = a.__next__()
+#         b1 = b.__next__()
+#         while a1 and b1:
+#             if a1 > b1:
+#                 b1 = next(b, None)
+#             elif a1 < b1:
+#                 a1 = next(a, None)
+#             else:
+#                 result.append(a1)
+#                 a1 = next(a, None)
+#                 b1 = next(b, None)
+#         return intersect(*([result] + list(postings[2:])))
+
+def ranking_query(query):
+    pattern = re.compile(r"[a-zA-Z0-9]+")
+    stemmer = SnowballStemmer("english")
+    with shelve.open(os.path.join("./index/record2" , 'inverted_index'), flag='c', writeback=True) as index:
+        with shelve.open(os.path.join("./index/record2", 'id_url'), flag='c') as mapping:
+            token_list = set()
+            for t in pattern.finditer(query):
+                word = stemmer.stem(t.group(0))
+                if word in index:
+                    token_list.add(word)
+                else:
+                    return None
+            # bool_result = intersect(*sorted(token_list, key= lambda x:len(x)))
+            # print(bool_result)
+            heap = []
+            for d in mapping:
+                contain = 0
+                score = 0
+                for term in token_list:
+                    if d in index[term]:
+                        contain += 1
+                        score += index[term][d]["w"] * index[term]["idf"]
+                if contain/len(token_list) >= 0.75:
+                    heapq.heappush(heap, (-score, d))
+            for score, id in heap[:10]:
+                print(id, ": ", mapping[id], "with score: ",-score, end = "\n")
+
 query = input("Please enter your query: ")
-pattern = re.compile(r"[a-zA-Z0-9]+")
-stemmer = SnowballStemmer("english")
-q = stemmer.stem(query.split()[0])
-print(q)
-l = len(query.split())
-print(l)
-with shelve.open(os.path.join("./index/record2" , 'inverted_index'), flag='c', writeback=True) as index:
-    with shelve.open(os.path.join("./index/record2", 'id_url'), flag='c') as mapping:
-        length = 0
-        token_list = []
-        for t in pattern.finditer(query):
-            word = stemmer.stem(t.group(0))
-            token_list.append(word)
-            length += index[word]["idf"] ** 2
-        length = math.sqrt(length)
-        heap = []
-        v = array()
-        for d in mapping:
-            contain = 0
-            score = 0
-            for term in pattern.finditer(query):
-                word = stemmer.stem(term.group(0))
-                if d in index[word]:
-                    contain += 1
-                    v.append(index[word][d]["w"] * index[word]["idf"] / length)
-            ratio = contain/l
-            if ratio >= 0.75:
-                heapq.heappush(heap, (-ratio,d))
-        for i in heap[:10]:
-            print(i, end = "\n")
+ranking_query(query)
